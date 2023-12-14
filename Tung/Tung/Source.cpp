c4114f45e363f02df2a1410e653b9f7c8725e41f
conankurito <conankurito@gmail.com>
2023-12-14 10:38:43 +0700

    dua source len

---
+++ Tung/Tung/Source.cpp
@@ -0,0 +1,10 @@
+﻿#include"Header.h"
+
+void nhapSo() {
+	for (int i = 1; i <= 5; i++) {
+		cout << "Chay for ngoai lan " << i << " -> ";
+		for (int j = 1; j <= 5; j++)
+			cout << "Chay for trong lan " << j << " -> ";
+		cout << "\n\n\n";
+	}
+}
